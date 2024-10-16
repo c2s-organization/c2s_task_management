249248207108e51653be349c6251a5532ad7f97f
Marco Castro <marcodotcastro@gmail.com>
2024-10-16 15:41:05 -0300

    test: model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,23 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'é válido com um título, status e URL' do
-    task = build(:task)
-    expect(task).to be_valid
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:url) }
   end
 
-  it 'é inválido sem um título' do
-    task = build(:task, title: nil)
-    expect(task).not_to be_valid
-  end
-
-  it 'é inválido sem um status' do
-    task = build(:task, status: nil)
-    expect(task).not_to be_valid
-  end
-
-  it 'é inválido sem uma URL' do
-    task = build(:task, url: nil)
-    expect(task).not_to be_valid
+  describe 'enums' do
+    it { should define_enum_for(:status).with_values(
+      "pending" => "pending",
+      "in_progress" => "in_progress",
+      "completed" => "completed",
+      "failed" => "failed"
+    ).backed_by_column_of_type(:string) }
   end
 end
